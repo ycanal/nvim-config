62e54289bd4dca28a4087e29d4ac304e6c92a58f
Yoann Canal <yoann.canal@gmail.com>
2023-12-12 08:37:57 +0100

    verylazyfy treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
     },
+    event = { "VeryLazy" },
     build = ':TSUpdate',
     init = function()
         require('nvim-treesitter.configs').setup({
